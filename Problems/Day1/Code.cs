ac4f2fe908f4ddb77c650ffa33f56d2770da4a93
Brandon Woodford <brandon_woodford@solutiaconsulting.com>
2022-11-30 17:56:55 -0600

    Initial commit of Advent2022

---
+++ Problems/Day1/Code.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2022.Problems.Day1
+{
+    internal class Code
+    {
+        public Code() { }
+    }
+}
